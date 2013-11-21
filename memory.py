6af2663a18112ba860041b7381aa2509a33a8a03
Darren Frenkel <darrenfrenkel@gmail.com>
2013-11-22 09:53:08 +1100

    added counter functionality showing the turns played

--- memory.py
+++ memory.py
@@ -15,7 +15,7 @@ guess1 = 5000
 guess2 = 5000
 index1 = 5000
 index2 = 5000
-
+turns = 1
 
   
 
@@ -28,14 +28,15 @@ for x in y:
            
 
 def new_game():
+    global turns
+    turns = 0
     pass  
 
 
    
 # define event handlers
 def mouseclick(pos):
-    global click, exposed, state,guess1, guess2, index1, index2
-    # add game state logic here
+    global click, exposed, state,guess1, guess2, index1, index2, turns, hello
     click = pos[0] // 50
     if state == 0:
         for x in y:
@@ -43,50 +44,45 @@ def mouseclick(pos):
                 exposed.pop(x)
                 exposed.insert(x,True)
                 index1 = x                
-        state = 1 
-        guess1 = card_deck[click]
-
+                state = 1 
+                guess1 = card_deck[click]
     elif state == 1:
         for x in y:
             if click == x and exposed[x] == False:
                 exposed.pop(x)
                 exposed.insert(x,True)
                 index2 = x	                
-        state = 2 
-        guess2 = card_deck[click]
-        
+                state = 2 
+                guess2 = card_deck[click]    
     elif state == 2:
         if guess1 != guess2:
             exposed.pop(index1)
             exposed.insert(index1,False)
             exposed.pop(index2)
             exposed.insert(index2,False)
+            turns += 1
             for x in y:
                 if click == x and exposed[x] == False:
                     exposed.pop(x)
                     exposed.insert(x,True)
                     index1 = x
-            state = 1
-            guess1 = card_deck[click]
-
+                    state = 1
+                    guess1 = card_deck[click]   
         else:
             for x in y:
                 if click == x and exposed[x] == False:
                     exposed.pop(x)
                     exposed.insert(x,True)
                     index1 = x
-            state = 1
-            guess1 = card_deck[click]
-
-            
-                    
-
+                    state = 1
+                    guess1 = card_deck[click]
             
            
 
                          
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
+    label.set_text("Turns = " + str(turns))
     z = 0
     cardup_pos = -40
     for x in range(len(card_deck)):
@@ -96,13 +92,13 @@ def draw(canvas):
         elif exposed[x] == False:  
                 canvas.draw_polygon(carddown_pos[z], 49, 'Green')
         z += 1 
-# pass
+
 
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Restart", new_game)
-label = frame.add_label("Turns = 0")
+label = frame.add_label("Turns = " + str(turns))
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
